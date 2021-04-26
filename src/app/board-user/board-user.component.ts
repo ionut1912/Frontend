fb1f5c145d115beb96bb8bd273bcecfe88477555
nites <nitescucornelionut19@gmail.com>
2021-04-26 19:34:25 +0300

    Rezervation bug

---
+++ src/app/board-user/board-user.component.ts
@@ -0,0 +1,11 @@
+import {ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
+import {MediaMatcher} from "@angular/cdk/layout";
+
+@Component({
+  selector: 'app-board-user',
+  templateUrl: './board-user.component.html',
+  styleUrls: ['./board-user.component.css']
+})
+export class BoardUserComponent  {
+
+}
