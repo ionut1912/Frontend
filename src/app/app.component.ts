bea6b61de84296d7b251f296dcf7c8b8cfa93f8e
nites <nitescucornelionut19@gmail.com>
2021-07-03 17:25:18 +0300

    refactor validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
-
-
-
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +8,6 @@ import { Component } from '@angular/core';
 
 export class AppComponent  {
   title = 'LicentaFrontend';
+  asd;
 
- 
-  }
+}
